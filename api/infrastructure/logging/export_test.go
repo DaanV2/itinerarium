199257cab475ee14ae3516f02f2afba35548249a
DaanV2 <daanverstraten@hotmail.com>
2026-07-20 20:23:34 +0200

    refactor: removed useless tests

--- api/infrastructure/logging/export_test.go
+++
@@ -1,8 +0,0 @@
-package logging
-
-import "github.com/charmbracelet/log"
-
-// Apply is apply exported for external tests.
-func Apply(logger *log.Logger, reportCaller bool, level, format string) {
-	apply(logger, reportCaller, level, format)
-}
